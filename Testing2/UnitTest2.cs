19b0a6d4575bbc30ebc62b8b4606c4859028164b
Caleb Atsu <P2597727@my365.dmu.ac.uk>
2022-02-11 16:52:57 +0000

    created order and orderline class created test for order and orderline class to create an instance and make sure its not empty

---
+++ Testing2/UnitTest2.cs
@@ -0,0 +1,18 @@
+﻿using ClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Testing2
+{
+    [TestClass]
+    public class tstOrderLine
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {
+            clsOrderLine AnOrderLine = new clsOrderLine();
+            Assert.IsNotNull(AnOrderLine);
+
+        }
+    }
+}
