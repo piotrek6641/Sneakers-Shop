474808caa4d60de7113aea11aca80ffd56754d9b
P2616423 <dnaid4k@gmail.com>
2022-02-13 19:44:40 +0000

    added testing for customer class

---
+++ Testing5/TestCustomers.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using ClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Testing5
+{
+    [TestClass]
+	public class tstCustomers1
+	{
+		[TestMethod]
+		public void TestMethodok()
+		{
+			clsCustomers AnCustomers = new clsCustomers();
+			Assert.IsNotNull(AnCustomers);
+		}
+	}
+}
---
+++ Testing5/tstCustomers.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using ClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Testing5
+{
+	[TestClass]
+	public class tstCustomers
+	{
+		[TestMethod]
+		public void TestMethod1()
+		{
+			clsCustomers AnCustomers = new clsCustomers();
+			Assert.IsNotNull(AnCustomers);
+		}
+	}
+}
