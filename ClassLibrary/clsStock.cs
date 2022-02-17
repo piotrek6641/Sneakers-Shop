c4c81d001387c820468902cb44d409127f2c8537
nmatynia <norbert.matyniaa@gmail.com>
2022-02-17 17:40:12 +0000

    Added tests to tstStock and created properties to clsStock class.

--- ClassLibrary/clsStock.cs
+++ ClassLibrary/clsStock.cs
@@ -1,6 +1,13 @@
-﻿namespace ClassLibrary
+﻿using System;
+
+namespace ClassLibrary
 {
     public class clsStock
     {
+        public bool Available { get; set; }
+        public DateTime DateAdded { get; set; }
+        public string SneakerName { get; set; }
+        public int SneakerNo { get; set; }
+        public int Price { get; set; }
     }
 }
--- Testing3/tstStock .cs
+++ Testing3/tstStock .cs
@@ -13,5 +13,45 @@ namespace Testing3
             clsStock AStock = new clsStock();
             Assert.IsNotNull(AStock);
         }
+
+        public void AvailablePropertyOK()
+        {
+            clsStock AStock = new clsStock();
+            Boolean TestData = true;
+            AStock.Available = TestData;
+            Assert.AreEqual(AStock.Available, TestData);
+        }
+
+        public void DateAddedOK()
+        {
+            clsStock AStock = new clsStock();
+            DateTime TestData = DateTime.Now.Date;
+            AStock.DateAdded = TestData;
+            Assert.AreEqual(AStock.DateAdded, TestData);
+        }
+
+        public void SneakerNamePropertyOK()
+        {
+            clsStock AStock = new clsStock();
+            string TestData = "Air Jordan 1";
+            AStock.SneakerName = TestData;
+            Assert.AreEqual(AStock.SneakerName, TestData);
+        }
+
+        public void SneakerNoPropertyOK()
+        {
+            clsStock AStock = new clsStock();
+            Int32 TestData = 1;
+            AStock.SneakerNo = TestData;
+            Assert.AreEqual(AStock.SneakerNo, TestData);
+        }
+
+        public void PricePropertyOK()
+        {
+            clsStock AStock = new clsStock();
+            Int32 TestData = 60;
+            AStock.Price = TestData;
+            Assert.AreEqual(AStock.Price, TestData);
+        }
     }
 }
