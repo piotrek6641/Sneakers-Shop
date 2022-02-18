7ab01647a23003ba8db04c6ba689ce44e65e9108
P2613576 <P2613576@CLPX0027WIN019W>
2022-02-18 12:45:54 +0000

    Added testing for Login class

---
+++ Testing1/tstStaff.cs
@@ -0,0 +1,17 @@
+﻿using ClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Testing1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {
+            clcLogin ALoggin = new clsLogin();
+            Assert.isNotNull(ALoggin);
+        }
+    }
+}
