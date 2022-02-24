fe6bb2dfcf5bd513fa8768bc53a7403ade987779
nmatynia <norbert.matyniaa@gmail.com>
2022-02-24 18:05:21 +0000

    Filled in StockDataEntry with attributes and edited StockViewer logic.

--- AdminSystem/StockDataEntry.aspx.cs
+++ AdminSystem/StockDataEntry.aspx.cs
@@ -4,11 +4,32 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using ClassLibrary;
 
 public partial class _1_DataEntry : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
 
+    }
+
+    protected void btnOK_Click(object sender, EventArgs e)
+    {
+        //create a new instance of clsStock
+        clsStock AStock = new clsStock();
+
+        //capture the Sneaker Name
+        AStock.SneakerName = txtSneakerName.Text;
+
+        //capture rest attributes
+        AStock.DateAdded = DateTime.Parse(txtDateAdded.Text);
+        AStock.Price = Int32.Parse(txtPrice.Text);
+        AStock.SneakerNo = Int32.Parse(txtSneakerNo.Text);
+
+        //store the address in the session object
+        Session["AStock"] = AStock;
+
+        //navigate to the viewer page
+        Response.Redirect("StockViewer.aspx");
     }
 }
