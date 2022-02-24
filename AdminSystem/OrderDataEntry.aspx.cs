a53852db6f7b5280ffbd267de40b2899bd3d0cdc
P2616423 <dnaid4k@gmail.com>
2022-02-24 20:12:36 +0000

    added a basic presentation layer for customer

--- AdminSystem/OrderDataEntry.aspx.cs
+++ AdminSystem/OrderDataEntry.aspx.cs
@@ -4,11 +4,12 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using ClassLibrary;
 
 public partial class _1_DataEntry : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-
+      
     }
 }
--- Testing1/tstStaff.cs
+++ Testing1/tstStaff.cs
@@ -1,17 +1,1 @@
-﻿using ClassLibrary;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System;
-
-namespace Testing1
-{
-    [TestClass]
-    public class UnitTest1
-    {
-        [TestMethod]
-        public void InstanceOK()
-        {
-            clcLogin ALoggin = new clsLogin();
-            Assert.isNotNull(ALoggin);
-        }
-    }
-}
+﻿
