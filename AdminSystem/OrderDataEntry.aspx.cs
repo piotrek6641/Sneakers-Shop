d9404e49c837ff30ad6830dfa4751c9103acb52f
Caleb Atsu <P2597727@my365.dmu.ac.uk>
2022-02-27 12:35:35 +0000

    captured rest of the data

--- AdminSystem/OrderDataEntry.aspx.cs
+++ AdminSystem/OrderDataEntry.aspx.cs
@@ -15,9 +15,20 @@ public partial class _1_DataEntry : System.Web.UI.Page
 
     protected void btnOK_Click(object sender, EventArgs e)
     {
+        //create a new instance of clsStock
         clsOrder AnOrder = new clsOrder();
-        AnOrder.OrderId = txtOrderId.Text;
+        AnOrder.OrderId = Int32.Parse(txtOrderId.Text);
+
+        // capturing the rest of the attributes
+        AnOrder.CustomerId = Int32.Parse(txtCustomerId.Text);
+        //AnOrder.Statues = Int32.Parse(txtOrderStatues.Text);
+        AnOrder.DateAdded = DateTime.Parse(txtOrderDate.Text);
+        AnOrder.StaffId = Int32.Parse(txtStaffId.Text);
+
+        //store the address in the session object
         Session["AnOrder"] = AnOrder;
+
+        //navigate to the viewer page
         Response.Redirect("OrderViewer.aspx");
     }
 }
--- AdminSystem/OrderViewer.aspx.cs
+++ AdminSystem/OrderViewer.aspx.cs
@@ -10,8 +10,19 @@ public partial class _1Viewer : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
+        //create a new instance of clsStock
         clsOrder AnOrder = new clsOrder();
+
+        //get the data from the session object
         AnOrder = (clsOrder)Session["AnOrder"];
+
+        //display the data for this entry
         Response.Write(AnOrder.OrderId);
+        Response.Write(AnOrder.CustomerId);
+        //Response.Write(AnOrder.Statues);
+        Response.Write(AnOrder.DateAdded);
+        Response.Write(AnOrder.StaffId);
     }
+
+
 }
