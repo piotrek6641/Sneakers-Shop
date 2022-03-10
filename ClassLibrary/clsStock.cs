1597e1997460d56152665ac2b869574d76637e85
nmatynia <norbert.matyniaa@gmail.com>
2022-03-10 18:12:19 +0000

    Initilize Find() method in clsStock and created new tests in tstStock.

--- ClassLibrary/clsStock.cs
+++ ClassLibrary/clsStock.cs
@@ -4,10 +4,75 @@ namespace ClassLibrary
 {
     public class clsStock
     {
-        public bool Available { get; set; }
-        public DateTime DateAdded { get; set; }
-        public string SneakerName { get; set; }
-        public int SneakerNo { get; set; }
-        public int Price { get; set; }
+        private Int32 mSneakerNo;
+        private DateTime mDateAdded;
+        private Boolean mAvailable;
+        private Int32 mPrice;
+        private String mSneakerName;
+
+        public int SneakerNo
+        {
+            get
+            {
+                return mSneakerNo;
+            }
+            set
+            {
+                mSneakerNo = value;
+            }
+        }
+        public bool Available 
+        {
+            get
+            {
+                return mAvailable;
+            }
+            set
+            {
+                mAvailable = value;
+            }
+        }
+        public DateTime DateAdded { 
+            get 
+            {
+                return mDateAdded;
+            } 
+            set 
+            {
+                mDateAdded = value;
+            } 
+        }
+        public string SneakerName 
+        {
+            get
+            {
+                return mSneakerName;
+            }
+            set
+            {
+                mSneakerName = value;
+            }
+        }
+        public int Price 
+        {
+            get
+            {
+                return mPrice;
+            }
+            set
+            {
+                mPrice = value;
+            }
+        }
+
+        public bool Find(int SneakerNo)
+        {
+            mSneakerNo = 21;
+            mDateAdded = Convert.ToDateTime("16/9/2015");
+            mAvailable = true;
+            mPrice = 100;
+            mSneakerName = "Air Jordan 1";
+            return true;
+        }
     }
 }
