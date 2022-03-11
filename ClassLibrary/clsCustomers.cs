346164bed1da2dbb3940f907cf686a906f89349f
P2616423 <dnaid4k@gmail.com>
2022-03-11 19:02:01 +0000

    Created new methods

--- ClassLibrary/clsCustomers.cs
+++ ClassLibrary/clsCustomers.cs
@@ -4,11 +4,86 @@ namespace ClassLibrary
 {
     public class clsCustomers
     {
+        private Int32 mCustomerid;
+        private String mCustomer_email;
+        private String mAddress;
+        private String mPhone;
+        private DateTime mDateAdded;
         public bool Specialoffers { get; set; }
-        public DateTime DateAdded { get; set; }
-        public int Customerid { get; set; }
-        public string Customer_email { get; set; }
-        public string Address { get; set; }
-        public string Phone { get; set; }
+        public DateTime DateAdded
+        {
+            get
+            {
+                return mDateAdded;
+            }
+            set
+            {
+                mDateAdded = value;
+            }
+        }
+        public Int32 Customerid
+        {
+            get
+            {
+                return mCustomerid;
+            }
+            set
+            {
+                mCustomerid = value;
+            }
+        }
+        public string Customer_email
+        {
+            get
+            {
+                return mCustomer_email;
+            }
+            set
+            {
+                mCustomer_email = value;
+            }
+        }
+        public string Address
+        {
+            get
+            {
+                return mAddress;
+            }
+            set
+            {
+                mAddress = value;
+            }
+        }
+        public string Phone
+        {
+            get
+            {
+                return mPhone;
+            }
+            set
+            {
+                mPhone = value;
+            }
+        }
+
+
+
+        public bool Find(int customerid)
+        {
+            mCustomerid = 2;
+            mDateAdded = Convert.ToDateTime("2-10-2022");
+           
+            return true;
+        }
+
+        public bool Find(string customer_email)
+        {
+            mCustomer_email = "norb@gmail.com";
+            mAddress = "Leicester le27df jarrom street 44";
+            mPhone = "07332986400";
+            return true;
+        }
+       
+
     }
 }
