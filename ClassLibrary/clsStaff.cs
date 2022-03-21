f72c6a80885a0ea75a444a3b9c3e8b9fb5f7899e
P2613576 <P2613576@CLPX0027WIN019W>
2022-03-21 12:17:08 +0000

    added data base communication between tests and database

--- ClassLibrary/clsStaff.cs
+++ ClassLibrary/clsStaff.cs
@@ -4,12 +4,90 @@ namespace ClassLibrary
 {
     public class clsStaff
     {
+        private string Login;
+        private DateTime mDateAdded;
+        private string Password;
+        private Boolean isAdmin;
+        private string email;
         public bool Active { get; set; }
-        public DateTime DateAdded { get; set; }
+        public DateTime DateAdded
+        {
+            get
+            {
+                return mDateAdded;
+            }
+            set
+            {
+                mDateAdded = value;
+            }
+        }
         public DateTime DateCreated { get; set; }
-        public bool StaffIsAdmin { get; set; }
-        public string StaffPassword { get; set; }
-        public string StaffLogin { get; set; }
-        public string StaffEmail { get; set; }
+        public bool StaffIsAdmin
+        {
+            get
+            {
+                return isAdmin;
+            }
+            set
+            {
+                isAdmin = value;
+            }
+        }
+        public string StaffPassword
+        {
+            get
+            {
+                return Password;
+            }
+            set
+            {
+                Password = value;
+            }
+        }
+        public string StaffLogin
+        {
+            get
+            {
+                return Login;
+            }
+            set
+            {
+                Login = value;
+            }
+        }
+            
+        public string StaffEmail
+        {
+            get
+            {
+                return email;
+            }
+            set
+            {
+                email = value;
+            }
+        }
+
+        public bool Find(string Login)
+        {
+            clsDataConnection DB = new clsDataConnection();
+            DB.AddParameter("@login", login);
+            DB.Execute("sproc_tblStaff_FilterByLogin");
+            if(DB.Count == 1)
+            {
+                Login = Convert.ToString(DB.DataTable.Rows[0]["Login"]);
+                Password = Convert.ToString(DB.DataTable.Rows[0]["Password"]);
+                email = Convert.ToString(DB.DataTable.Rows[0]["StaffEmail"]);
+                isAdmin = Convert.ToBoolean(DB.DataTable.Rows[0]["isAdmin"]);
+                mDateAdded = Convert.ToDateTime(DB.DataTable.Rows[0]["DateCreated"]);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+
+        }
     }
 }
