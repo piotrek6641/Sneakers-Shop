f4d75c1b786896ee151e43c9a3c738947f1bc95b
P2616423 <dnaid4k@gmail.com>
2022-03-21 12:47:19 +0000

    Created find method for customers

--- AdminSystem/CustomerDataEntry.aspx.cs
+++ AdminSystem/CustomerDataEntry.aspx.cs
@@ -20,9 +20,34 @@ public partial class _1_DataEntry : System.Web.UI.Page
         clsCustomers AnCustomers = new clsCustomers();
         //capture customer email
         AnCustomers.Customer_email = txtCustomer_email.Text;
+        //capture rest attribute
+        AnCustomers.DateAdded = DateTime.Parse(txtDateAdded.Text);
+        AnCustomers.Address = txtAddress.Text;
+        AnCustomers.Phone = txtPhone.Text;
+        AnCustomers.Customerid = Int32.Parse(txtCustomerid.Text);
+
         //store the address in the session object
         Session["AnCustomers"] = AnCustomers;
         //navigate to the viewer page
         Response.Redirect("CustomerViewer.aspx");
+    }
+    protected void btnFind_Click(object sender, EventArgs e)
+    {
+        clsCustomers AnCustomers = new clsCustomers();
+        Int32 Customerid;
+        //variable to store the result of the find operation
+        Boolean Found = false;
+
+        Customerid = Convert.ToInt32(txtCustomerid.Text);
+        Found = AnCustomers.Find(Customerid);
+
+        if (Found == true)
+        {
+            txtCustomer_email.Text = AnCustomers.Customer_email;
+            txtDateAdded.Text = AnCustomers.DateAdded.ToString();
+            txtAddress.Text = AnCustomers.Address;
+            txtPhone.Text = AnCustomers.Phone;
+            
+        }
     }
 }
