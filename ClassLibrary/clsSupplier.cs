ff9be9f29de9fc2e4a90d6a36ae9163626d935ff
tayy21 <P2604360@my365.dmu.ac.uk>
2022-05-06 02:27:43 +0100

    added hard coded data for tests

--- ClassLibrary/clsSupplier.cs
+++ ClassLibrary/clsSupplier.cs
@@ -4,27 +4,101 @@ namespace ClassLibrary
 {
     public class clsSupplier
     {
-        public int SupplierID
+        private Int32 tSupplierID;
+        public Int32 SupplierID
         {
             get
             {
-                return SupplierID;
+                return tSupplierID;
             }
             set
             {
-                SupplierID = value;
+                tSupplierID = value;
             }
         }
-        public bool InStock { get; set; }
-        public DateTime DeliveryDate { get; set; }
-        public string SupplierEmail { get; set; }
-        public string Address { get; set; }
-        public string PhoneNo { get; set; }
-        public int StockAmount { get; set; }
+        private bool tInStock;
+        public bool InStock
+        {
+            get
+            {
+                return tInStock;
+            }
+            set
+            {
+                tInStock = value;
+            }
+        }
+        private DateTime tDeliveryDate;
+        public DateTime DeliveryDate
+        {
+            get
+            {
+                return tDeliveryDate;
+            }
+            set
+            {
+                tDeliveryDate = value;
+            }
+        }
+        private string tSupplierEmail;
+        public string SupplierEmail
+        {
+            get
+            {
+                return tSupplierEmail;
+            }
+            set
+            {
+                tSupplierEmail = value;
+            }
+        }
+        private string tAddress;
+        public string Address
+        {
+            get
+            {
+                return tAddress;
+            }
+            set
+            {
+                tAddress = value;
+            }
+        }
+        private string tPhoneNo;
+        public string PhoneNo
+        {
+            get
+            {
+                return tPhoneNo;
+            }
+            set
+            {
+                tPhoneNo = value;
+            }
+        }
+        private Int32 tStockAmount;
+        public Int32 StockAmount
+        {
+            get
+            {
+                return tStockAmount;
+            }
+            set
+            {
+                tStockAmount = value;
+            }
+        }
+        
 
         public bool Find(int SupplierID)
         {
-            SupplierID = 21;
+            tSupplierID = 21;
+            tInStock = true;
+            tDeliveryDate = Convert.ToDateTime("31/10/2001");
+            tSupplierEmail = "supply@gmail.com";
+            tAddress = "6 new street";
+            tPhoneNo = "07780367982";
+            tStockAmount = 500;
             return true;
         }
     }
